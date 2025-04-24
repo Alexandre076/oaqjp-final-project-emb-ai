aa1bdd0e356468a6e4644baedbfd835afd3d3929
Alexandre076 <all.martins076@gmail.com>
2025-04-24 15:46:57 -0400

    Static code analysis performed with PyLint

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -12,28 +12,37 @@ def emotion_detector(text_to_analyse):
 
     # Sending a POST request to the sentiment analysis API
     response = requests.post(url, json=myobj, headers=header)
+    if response.status_code == 400:
+       result = {
+        'anger': None,
+        'disgust': None,
+        'fear': None,
+        'joy': None,
+        'sadness': None,
+        'dominant_emotion': None
+        }
+    elif response.status_code == 200:
+        # Parsing the JSON response from the API
+        emotion_predictions = json.loads(response.text)
 
-    # Parsing the JSON response from the API
-    emotion_predictions = json.loads(response.text)
+        #Extract the emotion scores
+        anger_score = emotion_predictions['emotionPredictions'][0]['emotion']['anger']
+        disgust_score = emotion_predictions['emotionPredictions'][0]['emotion']['disgust']
+        fear_score = emotion_predictions['emotionPredictions'][0]['emotion']['fear']
+        joy_score = emotion_predictions['emotionPredictions'][0]['emotion']['joy']
+        sadness_score = emotion_predictions['emotionPredictions'][0]['emotion']['sadness']
 
-    #Extract the emotion scores
-    anger_score = emotion_predictions['emotionPredictions'][0]['emotion']['anger']
-    disgust_score = emotion_predictions['emotionPredictions'][0]['emotion']['disgust']
-    fear_score = emotion_predictions['emotionPredictions'][0]['emotion']['fear']
-    joy_score = emotion_predictions['emotionPredictions'][0]['emotion']['joy']
-    sadness_score = emotion_predictions['emotionPredictions'][0]['emotion']['sadness']
+        #Determine the dominante emotion
+        dominant_emotion = max(emotion_predictions['emotionPredictions'][0]['emotion'], key=emotion_predictions['emotionPredictions'][0]['emotion'].get)
 
-    #Determine the dominante emotion
-    dominant_emotion = max(emotion_predictions['emotionPredictions'][0]['emotion'], key=emotion_predictions['emotionPredictions'][0]['emotion'].get)
-
-    result = {
-    'anger': anger_score,
-    'disgust': disgust_score,
-    'fear': fear_score,
-    'joy': joy_score,
-    'sadness': sadness_score,
-    'dominant_emotion': dominant_emotion
-    }
+        result = {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score,
+        'dominant_emotion': dominant_emotion
+        }
 
     #Return output formated
     return result
--- server.py
+++ server.py
@@ -1,22 +1,41 @@
+"""
+This module provides a Flask web application for emotion detection.
+"""
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask("emotionDetector")
 
 @app.route("/emotionDetector")
-def emotion_detector():
+def emot_detector():
+    """
+    Endpoint to detect emotions from the given text.
+    """
     # Retrieve the text to analyze from the request arguments
     text_to_analyze = request.args.get('textToAnalyze')
 
     # Pass the text to the emotion_detector function and store the response
     response = emotion_detector(text_to_analyze)
 
-    # Return a formatted string with the emotion detection
-    output = f"For the given statement, the system response is 'anger': {response['anger']}, 'disgust': {response['disgust']}, 'fear': {response['fear']}, 'joy': {response['joy']} and 'sadness': {response['sadness']}. The dominant emotion is {response['dominant_emotion']}."
+    if response['dominant_emotion'] is None:
+        output =  "Invalid text! Please try again!"
+    else:
+        # Return a formatted string with the emotion detection
+        output = (
+            f"For the given statement, the system response is 'anger': {response['anger']}, "
+            f"'disgust': {response['disgust']}, 'fear': {response['fear']}, "
+            f"'joy': {response['joy']} "
+            f"and 'sadness': {response['sadness']}. "
+            f"The dominant emotion is {response['dominant_emotion']}."
+        )
+
     return output
 
 @app.route("/")
 def render_index_page():
+    """
+    Render the index page.
+    """
     return render_template('index.html')
 
 if __name__ == "__main__":
